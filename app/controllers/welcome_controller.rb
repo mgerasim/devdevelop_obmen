2e00e6aad8facea68eef56db8c229f0ece99abe2
Mikhail Gerasimov <mgerasim@inbox.ru>
2018-05-25 19:06:53 +1000

    выполнение верстки

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,11 @@
 class WelcomeController < ApplicationController
   def index
     @cryptocurrencies =Cryptocurrency.all
-    @current = params[:currency]
-    if @current == nil then
-	@current = Cryptocurrency.first.id
+    
+    if params[:currency] == nil then
+		@current = Cryptocurrency.first
+    else
+    	@current = Cryptocurrency.find(params[:currency])
     end
   end
 end
