dc93f1786139a29675faa6677f345604198e5b7a
Mikhail Gerasimov <mgerasim@inbox.ru>
2018-05-30 16:15:27 +1000

    default_options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
 	include SessionHelper
 	  before_action :require_login
+	def default_options
+		if Rails.env.production?
+      		{ :port => 81}
+      	else
+      		{ :port => 3000}
+    	end
+	end
+
 	def default_url_options
     	if Rails.env.production?
       		{ :port => 81}
