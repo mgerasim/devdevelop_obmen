78caa87e81013094ff2a6addf23612a12c972a59
Mikhail Gerasimov <mgerasim@inbox.ru>
2018-06-27 20:10:04 +1000

    Загрузить новый шаблон #18

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,17 @@
+function bt1(){
+	document.getElementById('b1').style.color='#22b7da';
+	document.getElementById('b2').style.color='#fff';
+	document.getElementById('b3').style.color='#fff';
+}
+
+function bt2(){
+	document.getElementById('b1').style.color='#fff';
+	document.getElementById('b2').style.color='#22b7da';
+	document.getElementById('b3').style.color='#fff';
+}
+
+function bt3(){
+	document.getElementById('b1').style.color='#fff';
+	document.getElementById('b2').style.color='#fff';
+	document.getElementById('b3').style.color='#22b7da';
+}
